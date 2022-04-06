03eb2bbec5c740e139441ff03f3918e63c091633
unknown <alexandermjung89@gmail.com>
2022-04-06 17:00:56 -0500

    working through the step by step tutorial done with chapter 3

--- math-practice/src/App.js
+++ math-practice/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const topBar = (
+  <div>
+    <h1>Hello.</h1>
+    <h6>we've missed you</h6>
+</div>
+);
+
 function App() {
   return (
     <div className="App">
+    {topBar}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
